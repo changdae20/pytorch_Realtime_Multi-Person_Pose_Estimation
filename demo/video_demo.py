4fe84621b580d98c610ba26573b56bba218c34d7
changdae20 <changdae20@naver.com>
2022-01-19 15:47:14 +0900

    MultiGPU Inference

--- demo/video_demo.py
+++ demo/video_demo.py
@@ -30,19 +30,21 @@ args["weight"] = 'pose_model.pth'
 args["opts"] = []
 update_config(cfg, args)
 
+batch_size = 400
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 model = get_model('vgg19')     
 model.load_state_dict(torch.load(args["weight"]))
-model = torch.nn.DataParallel(model).cuda()
-model.float()
+model = torch.nn.DataParallel(model).to(device)
 model.eval()
 avg_time = 0
 total_start = time.time()
 for filename in os.listdir("videos/"):
     test_video = "videos/"+filename
     cap = cv2.VideoCapture(test_video) # B,G,R order
-    fps = cap.get(cv2.CAP_PROP_FPS)
+    #fps = cap.get(cv2.CAP_PROP_FPS)
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    #fps=24
+    fps=24
     print("fps : ", fps)
     print("length : ", length)
     width, height = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -51,30 +53,45 @@ for filename in os.listdir("videos/"):
     # Get results of original image
     count = 0
     while(cap.isOpened()):
-        ret, frame = cap.read()
+        frames = []
+        frames_orig = []
+        start = time.time()
+        for _ in range(batch_size):
+            ret, frame = cap.read()
+            if not ret:
+                cap.release()
+                break
+            im_croped, _, _ = im_transform.crop_with_factor(
+                frame, cfg.DATASET.IMAGE_SIZE, factor=cfg.MODEL.DOWNSAMPLE, is_ceil=True)
+            frames.append(im_croped)
+            frames_orig.append(frame)
+        if len(frames)==0: break
+        frames = np.stack(frames) # Shape : (B,H,W,C)
+        #print(np.shape(frames))
 
-        if ret:
-            start = time.time()
-            remain_time = avg_time * (length-count)
-            
-            sys.stdout.write(f"\r{'■'* (((count+1)*20)//length)}{' '* (20-((count+1)*20)//length)} ({count+1}/{length}) ETA : {int(remain_time)//3600:2d}:{(int(remain_time)%3600)//60:2d}:{int(remain_time)%60:2d}")
-            with torch.no_grad():
-                paf, heatmap, im_scale = get_outputs(frame, model,  'rtpose')
-            #print(im_scale)
-            humans = paf_to_pose_cpp(heatmap, paf, cfg)
-            draw = draw_humans(frame, humans)
-            #cv2.imshow("result",draw)
-            #cv2.waitKey(1)
-            out.write(draw)
-            count += 1
-            end = time.time()
-            avg_time = ((end-start) + avg_time*(count-1))/count
-        else:
-            break
+        frames = frames.astype(np.float32)
+        frames = frames / 256. - 0.5
+        frames = frames.transpose((0, 3, 1, 2)).astype(np.float32) # Shape : (B,C,H,W)
+        print(np.shape(frames))
+
+        with torch.no_grad():
+            frames = torch.from_numpy(frames).float().to(device)
+            predicted_outputs, _ = model(frames)
+            output1, output2 = predicted_outputs[-2], predicted_outputs[-1]
+            heatmap = output2.cpu().data.numpy().transpose(0, 2, 3, 1)
+            paf = output1.cpu().data.numpy().transpose(0, 2, 3, 1)
+            for (f,p,h) in zip(frames_orig,paf,heatmap):
+                humans = paf_to_pose_cpp(h, p, cfg)
+                draw = draw_humans(f, humans)
+                out.write(draw)
+        
+        count += len(frames_orig)
+        end = time.time()
+        avg_time = ((end-start) + avg_time*(count-1))/count
 
     cap.release()
     out.release()
 
 total_end = time.time()
 
-print(f"\nTotal Inference Time : {total_end - total_start:.1f} s")
+print(f"\nTotal Inference Time : {(total_end - total_start):.1f} s")
